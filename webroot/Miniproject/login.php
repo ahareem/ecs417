ae817fd68b2fe487c4a7bbc495c5b890746792b0
ahareem <60136524+ahareem@users.noreply.github.com>
2020-04-21 03:54:28 +0100

    Add files via upload

---
+++ webroot/Miniproject/login.php
@@ -0,0 +1,27 @@
+<?php
+
+    include("config.php");
+    session_start();
+
+    if(isset ($_POST['email']) && isset ($POST['psw'])) //isset checks if the vars have been filled 
+    {
+
+        $query = "select * from USERS where email = '".$_POST['email']."' AND password = '".$_POST['psw']."'"; // CHECK FOR THE RECORD FROM TABLE * selects all fields
+
+        $result = mysqli_query($conn ,$query); //sends a query to the mysql database 
+
+
+        //if no of rows =1 in table (which it will as it will only contain one row of info (mine))
+        if(mysqli_fetch_assoc($result))
+        {
+            header("Location: addPost.html");
+            exit();
+        }
+        else
+        {
+            header("Location: index.html");
+            exit();
+        }
+    }
+
+?>
