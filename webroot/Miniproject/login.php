2255c1b071d51862f77f740fd4c39ce17b56bc66
ahareem <60136524+ahareem@users.noreply.github.com>
2020-04-21 20:37:38 +0100

    Update login.php

--- webroot/Miniproject/login.php
+++ webroot/Miniproject/login.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once("config.php");
+    include("config.php");
     session_start();
 
     if(isset ($_POST['email']) && isset ($POST['psw'])) //isset checks if the vars have been filled 
