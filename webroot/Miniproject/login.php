976d6f330f35163f596ca93e944b0a570029ca19
ahareem <60136524+ahareem@users.noreply.github.com>
2020-04-21 20:48:05 +0100

    Update login.php

--- webroot/Miniproject/login.php
+++ webroot/Miniproject/login.php
@@ -6,7 +6,7 @@
     if(isset ($_POST['email']) && isset ($POST['psw'])) //isset checks if the vars have been filled 
     {
 
-        $query = "select * from USERS where email = '".$_POST['email']."' AND password = '".$_POST['psw']."'"; // CHECK FOR THE RECORD FROM TABLE * selects all fields
+        $sql = "select * from USERS where email = '".$_POST['email']."' AND password = '".$_POST['psw']."'"; // CHECK FOR THE RECORD FROM TABLE * selects all fields
 
         $result = $conn->query($sql); //sends a query to the mysql database 
 
