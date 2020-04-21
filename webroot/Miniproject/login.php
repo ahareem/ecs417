8d5cb8bf1913dc5d9e7e8412a2c8667ecec530c2
ahareem <60136524+ahareem@users.noreply.github.com>
2020-04-21 16:27:20 +0100

    Update login.php

--- webroot/Miniproject/login.php
+++ webroot/Miniproject/login.php
@@ -6,12 +6,12 @@
     if(isset ($_POST['email']) && isset ($POST['psw'])) //isset checks if the vars have been filled 
     {
 
-        $query = "select * from USERS where email = '".$_POST['email']."' AND password = '".$_POST['psw']."'"; // CHECK FOR THE RECORD FROM TABLE * selects all fields
+        $sql = "select * from USERS where email = '".$_POST['email']."' AND password = '".$_POST['psw']."'"; // CHECK FOR THE RECORD FROM TABLE * selects all fields
 
         $result = mysqli_query($conn ,$query); //sends a query to the mysql database 
 
 
-        if(mysqli_num_rows($result) == 1)
+        if($conn->query($sql) === TRUE)
         {
             header("Location: /opt/app-root/src/webroot/Miniproject/addPost.html");
         }
