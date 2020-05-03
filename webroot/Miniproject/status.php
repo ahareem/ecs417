6b2cae29804363db25b4075aebe9b9e0b05d8918
ahareem <60136524+ahareem@users.noreply.github.com>
2020-05-03 01:23:25 +0100

    Add files via upload

---
+++ webroot/Miniproject/status.php
@@ -0,0 +1,15 @@
+<?php
+
+    session_start();
+    include("config.php");
+    if(isset($_SESSION['user']) && ($_SESSION['status'] == true)) 
+    {
+        header("Location: http://cakephp-mysql-persistent-webtechshit.bde1.qmul-eecs.openshiftapps.com/Miniproject/addPost.html");
+        exit();
+    }
+    else
+    {
+        header("Location: http://cakephp-mysql-persistent-webtechshit.bde1.qmul-eecs.openshiftapps.com/Miniproject/viewPost.php");
+        exit();
+    }
+?>
