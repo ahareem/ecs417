6375f9ae608642d3dc9fcb426c09de84b9c491a9
ahareem <60136524+ahareem@users.noreply.github.com>
2020-05-04 20:17:44 +0100

    Update and rename miniproject.html to index.php

--- webroot/Miniproject/index.php
+++ webroot/Miniproject/index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <link rel = "stylesheet" href = "reset.css" type = "text/css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel = "stylesheet" href = "miniproject.css" type = "text/css">
+        <link rel = "stylesheet" href = "index.css" type = "text/css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
